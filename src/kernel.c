2203eddf0d1ebe84f3fa381e170116ec6a0dfdfb
Daniel Thornburgh <mysterymath@gmail.com>
2023-11-25 13:23:19 -0800

    Remove padding from ELF file

--- src/kernel.c
+++ src/kernel.c
@@ -3,8 +3,7 @@
 extern char hello[];
 
 void main(void) {
-  // bit[0] of the address must be set to indicate thumb
   void (*_start)(void (*)(const char *fmt, ...)) = (void (*)(
-      void (*)(const char *fmt, ...)))((unsigned)hello + 0x8000 | 1);
+      void (*)(const char *fmt, ...)))(hello + 74 + 1);
   _start(printk);
 }
