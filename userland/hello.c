547e04327f6fd3bbf36088607d086a1e62d13371
Daniel Thornburgh <mysterymath@gmail.com>
2023-12-13 16:21:43 -0800

    Add constructor and destructor print

--- userland/hello.c
+++ userland/hello.c
@@ -1,5 +1,13 @@
 void printk(const char *fmt, ...);
 
+__attribute__((constructor)) void cons() {
+  printk("Hello constructor!\n");
+}
+
 void _start(void) {
   printk("Hello, world!\n");
+}
+
+__attribute__((destructor)) void dest() {
+  printk("Hello destructor!\n");
 }
