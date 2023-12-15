068a2e191a5656b0ddc5e178e4891a6cd28e3ba6
Daniel Thornburgh <mysterymath@gmail.com>
2023-12-15 10:35:49 -0800

    Update west.yml, toolchain dir

--- userland/cmake/toolchain.cmake
+++ userland/cmake/toolchain.cmake
@@ -2,7 +2,7 @@ cmake_minimum_required(VERSION 3.20)
 
 set(CMAKE_SYSTEM_NAME Microcosm)
 
-set(path ${CMAKE_CURRENT_LIST_DIR}/../../../llvm-project/install/bin)
+set(path ${CMAKE_CURRENT_LIST_DIR}/../../../toolchain/install/bin)
 
 set(CMAKE_ASM_COMPILER ${path}/clang CACHE FILEPATH "")
 set(CMAKE_C_COMPILER ${path}/clang CACHE FILEPATH "")
